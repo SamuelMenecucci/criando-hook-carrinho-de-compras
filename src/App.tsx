f487fb362bd230981edd50f5666e6ed581228cde
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2021-11-08 17:52:04 -0300

    productProvider removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
-import Routes from './routes';
-import GlobalStyles from './styles/global';
-import Header from './components/Header';
-import { CartProvider } from './hooks/useCart';
+import Routes from "./routes";
+import GlobalStyles from "./styles/global";
+import Header from "./components/Header";
+import { CartProvider } from "./hooks/useCart";
 
 const App = (): JSX.Element => {
   return (
